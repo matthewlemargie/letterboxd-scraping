7fe16a8470d8898f33b3824f2eab534b723ba1fb
Matthew Lemargie <matthewlemargie03@gmail.com>
2024-04-28 13:47:33 -0700

    new line

--- scraping.py
+++ scraping.py
@@ -6,6 +6,10 @@ driver = webdriver.Firefox()
 
 driver.get("https://www.letterboxd.com/films/popular/page/1/")
 
+elements = driver.find_elements("class_name", "frame")
+
+print(elements)
+
 time.sleep(5)
 
 driver.quit()
