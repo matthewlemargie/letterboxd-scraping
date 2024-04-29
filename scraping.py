117e9a9197e35572cf0ed7475780faa31fb192da
dankzappa <matthewlemargie03@gmail.com>
2024-04-28 23:01:36 -0700

    fix file appending

--- scraping.py
+++ scraping.py
@@ -4,25 +4,25 @@ from bs4 import BeautifulSoup
 import time
 import requests
 
-with open("data.csv", "a") as f:
-    for i in range(1,1001):
-        driver = webdriver.Firefox()
-        driver.get(f"https://www.letterboxd.com/films/popular/page/{i}/")
-        time.sleep(3)
-        elements = driver.find_elements(By.CLASS_NAME, "frame")
-        links = [x.get_attribute("href") for x in elements]
-        driver.quit()
-        links = [x for x in links if x is not None]
+for i in range(1,1001):
+    driver = webdriver.Firefox()
+    driver.get(f"https://www.letterboxd.com/films/popular/page/{i}/")
+    time.sleep(3)
+    elements = driver.find_elements(By.CLASS_NAME, "frame")
+    links = [x.get_attribute("href") for x in elements]
+    driver.quit()
+    links = [x for x in links if x is not None]
 
-        driver = webdriver.Firefox()
-        for link in links:
-            driver.get(link)
-            time.sleep(0.75)
-            soup = BeautifulSoup(driver.page_source, "html.parser")
-            title = soup.find("div", class_="col-17").find("h1").text
-            year = soup.find("div", class_="col-17").find("a").text
-            stars = soup.find("div", class_="rating-histogram clear rating-histogram-exploded").find_all("a")
-            stars = [star.text.split(" ")[-1] for star in stars]
+    driver = webdriver.Firefox()
+    for link in links:
+        driver.get(link)
+        time.sleep(0.75)
+        soup = BeautifulSoup(driver.page_source, "html.parser")
+        title = soup.find("div", class_="col-17").find("h1").text
+        year = soup.find("div", class_="col-17").find("a").text
+        stars = soup.find("div", class_="rating-histogram clear rating-histogram-exploded").find_all("a")
+        stars = [star.text.split(" ")[-1] for star in stars]
+        with open("data.csv", "a") as f:
             f.write(f"{title}, {year}, {stars[0]}, {stars[1]}, {stars[2]}, {stars[3]}, {stars[4]}, {stars[5]}, {stars[6]}, {stars[7]}, {stars[8]}, {stars[9]}\n")
 
-        driver.quit()
+    driver.quit()
