881c3f8813ddd2c4559ad5770284807c359a7d03
dankzappa <matthewlemargie03@gmail.com>
2024-04-29 22:31:17 -0700

    replace soy try/except statements with gigachad while loops to improve speed and avoid errors

--- scraping.py
+++ scraping.py
@@ -7,32 +7,26 @@ import requests
 file = open("data.csv", "w")
 file.close()
 
-driver = webdriver.Firefox()
-for i in range(1,1001):
+for i in range(1,501):
+    driver = webdriver.Firefox()
     driver.get(f"https://www.letterboxd.com/films/popular/page/{i}/")
-    time.sleep(2)
-    try:
-        elements = None
+    elements,links = None, None
+    while not elements or not links:
         elements = driver.find_elements(By.CLASS_NAME, "frame")
-        links = None
         links = [x.get_attribute("href") for x in elements]
-        links = [x for x in links if x is not None]
-    except:
-        continue
+    links = [x for x in links if x is not None]
 
     for link in links:
         driver.get(link)
-        time.sleep(0.7)
-        soup = BeautifulSoup(driver.page_source, "html.parser")
-        title = soup.find("div", class_="col-17").find("h1").text
-        year = soup.find("div", class_="col-17").find("a").text
-        try:
-            stars = None
-            stars = soup.find("div", class_="rating-histogram clear rating-histogram-exploded").find_all("a")
-            stars = [star.text for star in stars]
-            with open("data.csv", "a") as f:
-                f.write(f"{title}, {year}, {stars[0]}, {stars[1]}, {stars[2]}, {stars[3]}, {stars[4]}, {stars[5]}, {stars[6]}, {stars[7]}, {stars[8]}, {stars[9]}\n")
-        except:
-            pass
-driver.quit()
+        title, year, stars = None, None, None
+        while not title or not year or not stars:
+            soup = BeautifulSoup(driver.page_source, "html.parser")
+            title = soup.find("div", class_="col-17").find("h1").text
+            year = soup.find("div", class_="col-17").find("a").text
+            stars = soup.find("div", class_="rating-histogram clear rating-histogram-exploded")
+        stars = stars.find_all("a")
+        stars = [star.text for star in stars]
+        with open("data.csv", "a") as f:
+            f.write(f"{title}, {year}, {stars[0]}, {stars[1]}, {stars[2]}, {stars[3]}, {stars[4]}, {stars[5]}, {stars[6]}, {stars[7]}, {stars[8]}, {stars[9]}\n")
+    driver.quit()
 
